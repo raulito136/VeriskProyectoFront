536bb55b01e1dfe8c46db63a042d2c2f1ccce713
oscarcsg <velagomezoscar@gmail.com>
2026-05-05 11:13:51 +0200

    custom button ui component created

---
+++ libs/ui/src/lib/button/button.component.ts
@@ -0,0 +1,67 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+export type ButtonType = 'default' | 'confirm' | 'cancel' | 'submit' | 'custom';
+
+@Component({
+  selector: 'lib-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+})
+export class ButtonComponent {
+  @Input() type: ButtonType = 'default';
+  @Input() content: string = '';
+
+  // Output for handling the interaction with the button
+  @Output() buttonClick = new EventEmitter<void>();
+
+  baseClasses =
+    'px-5 py-2 rounded-lg font-bold shadow-md transition-all duration-150 hover:-translate-y-1 hover:shadow-xl';
+
+  // Set color depending on the button type
+  get colorClasses(): string {
+    switch (this.type) {
+      case 'default':
+        return 'bg-gray-300 hover:bg-gray-400';
+
+      case 'confirm':
+        return 'bg-lime-400 hover:bg-lime-500';
+
+      case 'cancel':
+        return 'bg-red-500 hover:bg-red-600';
+
+      case 'submit':
+        return 'bg-cyan-300 hover:bg-cyan-400';
+
+      default:
+        return 'bg-gray-300 hover:bg-gray-400';
+    }
+  }
+
+  // Button text content
+  get contentText(): string {
+    if (this.content) return this.content;
+
+    switch (this.type) {
+      case 'default':
+        return 'Continue';
+
+      case 'confirm':
+        return 'Confirm';
+
+      case 'cancel':
+        return 'Cancel';
+
+      case 'submit':
+        return 'Submit';
+
+      case 'custom':
+        return 'Custom Button';
+
+      default:
+        return 'Continue';
+    }
+  }
+}
--- mfe-shell/src/app/pages/welcome/welcome.component.ts
+++ mfe-shell/src/app/pages/welcome/welcome.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ButtonComponent } from '@policy-system/ui';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ButtonComponent],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css',
 })
